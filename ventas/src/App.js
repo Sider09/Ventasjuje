91c83c1519aa7b4f6086a349505e75ab14d613ea
Sider09 <fabriciobedoyajk@gmail.com>
2023-01-04 18:58:37 -0400

    cambio

--- ventas/src/App.js
+++ ventas/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          pRIMER EDITADO <code>src/App.js</code> TODAS PUTRAS
         </p>
         <a
           className="App-link"
